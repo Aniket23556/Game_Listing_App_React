e5620e33c19710aea3e10d65edcc868d058e92ed
Aniket Anand <aniket.23556@sscbs.du.ac.in>
2024-08-06 06:02:20 +0530

    Popular Section

---
+++ src/Components/GamesByGenresId.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+function GamesByGenresId({gameList}) {
+    useEffect(()=>{
+        console.log("GameList",gameList);
+        
+    },[])
+  return (
+    <div>
+        <h2 className='font-bold text-[30px] dark:text-white mt-5'>Popular Games</h2>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5'>
+        {gameList.map((item)=>{
+            <div className='bg-[#76a8f75e] p-4 rounded-lg h-full pb-10 hover:scale-110 transition-all duration-300 ease-in-out'>
+                <img src={item.background_image} alt="" className='w-full h-[80%] rounded-xl object-cover'/>
+                <h2 className='text-[20px] text-white'>{item.name}<span className='p-1 rounded-sm ml-2 text-[10px]
+                 bg-green-100 text-green-700 font-medium'>{item.metacritic}</span></h2>
+                <h2 className='text-gray-500 dark:text-gray-300'>
+                    ⭐{item.rating} 💬{item.reviews_count} 🔥{item.suggestions_count}
+                </h2>
+            </div>
+        })}
+    </div>
+    </div>
+  )
+}
+
+export default GamesByGenresId
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,13 +3,16 @@ import GenreList from '../Components/GenreList'
 import GlobalApi from '../Services/GlobalApi'
 import Banner from '../Components/Banner';
 import TrendingGames from '../Components/TrendingGames';
+import GamesByGenresId from '../Components/GamesByGenresId';
 
 function Home() {
 
   const [allGameList, setAllGameList]=useState();
+  const [gameListByGenres,setGameListByGenres]=useState([]);
 
   useEffect(()=>{
     getAllGamesList();
+    getGameListByGenresId(4);
   },[])
 
   const getAllGamesList=() =>{
@@ -18,16 +21,28 @@ function Home() {
     })
   }
 
+  const getGameListByGenresId=(id)=>{
+ 
+    GlobalApi.getGameListByGenreId(id).then((resp)=>{
+     
+      setGameListByGenres(resp.data.results)
+    })
+  }
+
   return (
     <div className='grid grid-cols-4 px-8'>
       <div className='h-screen hidden md:block px-8'>
-        <GenreList/>
+        <GenreList
+        genereId={(genereId)=>getGameListByGenresId(genereId)}
+        selectedGenresName={(name)=>setSelctedGenresName(name)}/>
       </div>
       <div className='col-span-4 md:col-span-3 px-4'>
         {allGameList?.length>0 ?
         <div>
         <Banner gameBanner={allGameList[0]}/>
         <TrendingGames gameList={allGameList}/>
+        <GamesByGenresId gameList={gameListByGenres} 
+        selctedGenresName={selctedGenresName}/>
         </div>
         :null}
       </div>
